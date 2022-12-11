e3a6d14a4ffec010bc04aa3d920f6c69a1f7616a
robert <robert>
2022-12-12 00:19:25 +0100

    Day1 (JS in Browser): Reducer approach

--- day1/js_in_browser/day1.js
+++ day1/js_in_browser/day1.js
@@ -6,6 +6,15 @@ function calc() {
 }
 
 function elvesCalories(inputValue) {
+    const imperativeResult = imperativeApproach(inputValue);
+    const reducerResult = reducerApproach(inputValue);
+    if (imperativeResult != reducerResult) {
+        return `[Error] imperativeResult: ${imperativeResult} != reducerResult: ${reducerResult}`;
+    }
+    return imperativeResult;
+}
+
+function imperativeApproach(inputValue) {
     let maxCalories = 0;
     let currentCalories = 0;
     inputValue.split('\n').forEach(line => {
@@ -18,4 +27,16 @@ function elvesCalories(inputValue) {
     });
 
     return maxCalories;
+}
+
+function reducerApproach(inputValue) {
+    const elvesCalories = inputValue.split('\n').reduce((acc, curr) => {
+        if (curr) {
+            acc[acc.length-1] += parseInt(curr);
+        } else {
+            acc.push(0);
+        }
+        return acc;
+    }, [0]);
+    return Math.max(...elvesCalories);
 }
