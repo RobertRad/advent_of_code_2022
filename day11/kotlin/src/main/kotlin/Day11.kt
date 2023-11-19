04b1908f893b920d2a3fba75118ce124a289c2c7
Robert <robert>
2023-11-19 21:14:30 +0100

    Day11 - Kotlin - Part2

--- day11/kotlin/src/main/kotlin/Day11.kt
+++ day11/kotlin/src/main/kotlin/Day11.kt
@@ -1,9 +1,10 @@
 fun main() {
     val content = getResource("input.txt")
-    val puzzle = parse(content)
-    println("puzzle: $puzzle")
-    println("Part1: ${part1(puzzle)}")
-//    println("Part2: ${part2(puzzle)}")
+    val monkeys = parse(content)
+
+    println("monkeys: $monkeys")
+    println("Part1: ${part1(monkeys)}")
+    println("Part2: ${part2(monkeys)}")
 }
 
 fun getResource(fileName: String): String {
@@ -22,12 +23,12 @@ fun parse(input: String): List<Monkey> {
     return monkeyDefinitions.map { monkeyDefinition ->
         val definition = monkeyDefinition.lines()
         val number = matchOrThrow(definition[0], """Monkey (\d+):""", "")[1].toInt()
-        val startingItems = definition[1].substring("  Starting items: ".length).split(", ").map { it.toInt() }
+        val startingItems = definition[1].substring("  Starting items: ".length).split(", ").map { it.toLong() }
         val operationMatch = matchOrThrow(definition[2], """  Operation: new = (old|\d+) (\+|\*) (old|\d+)""", "")
 
-        fun operation(old: Int): Int {
-            val operand1 = if (operationMatch[1] == "old") old else operationMatch[1].toInt()
-            val operand2 = if (operationMatch[3] == "old") old else operationMatch[3].toInt()
+        fun operation(old: Long): Long {
+            val operand1 = if (operationMatch[1] == "old") old else operationMatch[1].toLong()
+            val operand2 = if (operationMatch[3] == "old") old else operationMatch[3].toLong()
             return when (operationMatch[2]) {
                 "+" -> operand1 + operand2
                 "*" -> operand1 * operand2
@@ -42,7 +43,7 @@ fun parse(input: String): List<Monkey> {
         }
         val operationDescValue = if (operationMatch[3] == "old") "itself" else operationMatch[3]
         val operationDesc = "$operationDescOperand $operationDescValue"
-        val divisibleBy = definition[3].substring("  Test: divisible by ".length).toInt()
+        val divisibleBy = definition[3].substring("  Test: divisible by ".length).toLong()
         val ifTrue = definition[4].substring("    If true: throw to monkey ".length).toInt()
         val ifFalse = definition[5].substring("    If false: throw to monkey ".length).toInt()
 
@@ -50,35 +51,48 @@ fun parse(input: String): List<Monkey> {
     }
 }
 
-fun part1(monkeys: List<Monkey>): Int {
-    for (round in 1..20) {
+fun part1(monkeys: List<Monkey>) = solve(monkeys, true, 20) { it <= 10 || it == 15 || it == 20}
+
+fun part2(monkeys: List<Monkey>) = solve(monkeys, false, 10000) { it == 1 ||  it == 20 || it % 1000 == 0}
+
+fun solve(input: List<Monkey>, divideByThree: Boolean, numRounds: Int, whenToPrint: (Int) -> Boolean): Long {
+    val monkeys = input.map { it.copy() }
+    val factorOfAllDivisors = monkeys.map { it.divisibleBy }.reduce { acc, divisor -> acc * divisor }
+    println("factorOfAllDivisors: $factorOfAllDivisors")
+    for (round in 1..numRounds) {
         fun debugPrintln(msg: String) = if (round == 1) println(msg) else {}
         monkeys.forEachIndexed() { currentMonkeyIndex, monkey ->
             debugPrintln("Monkey ${monkey.number}:")
             monkey.items.forEach { item ->
                 debugPrintln("  Monkey inspects an item with a worry level of ${item}.")
-                val worryAfterInspection = monkey.operation.invoke(item)
-                debugPrintln("    Worry level ${monkey.operationDesc} to $worryAfterInspection.")
-                val worryAfterRelief = worryAfterInspection / 3
-                debugPrintln("    Monkey gets bored with item. Worry level is divided by 3 to $worryAfterRelief.")
-                val division = worryAfterRelief.toDouble().div(monkey.divisibleBy)
+                var worryLevel = monkey.operation.invoke(item)
+                debugPrintln("    Worry level ${monkey.operationDesc} to $worryLevel.")
+                if (divideByThree) {
+                    worryLevel /= 3
+                    debugPrintln("    Monkey gets bored with item. Worry level is divided by 3 to $worryLevel.")
+                } else {
+                    worryLevel %= factorOfAllDivisors
+                }
+                val division = worryLevel.toDouble().div(monkey.divisibleBy)
                 val testResult = division % 1 == 0.0
                 debugPrintln("    Current worry level is ${if (testResult) "" else "not"} divisible by ${monkey.divisibleBy}")
                 val nextMonkeyIndex = if (testResult) monkey.targetMonkeyIfTrue else monkey.targetMonkeyIfFalse
-                debugPrintln("    Item with worry level $worryAfterRelief is thrown to monkey $nextMonkeyIndex.")
+                debugPrintln("    Item with worry level $worryLevel is thrown to monkey $nextMonkeyIndex.")
                 if (currentMonkeyIndex == nextMonkeyIndex) {
                     throw IllegalStateException("Cannot throw item to one self")
                 }
-                monkeys[nextMonkeyIndex].items.add(worryAfterRelief)
+                monkeys[nextMonkeyIndex].items.add(worryLevel)
             }
             monkey.numberOfInspections += monkey.items.size
             monkey.items.clear()
         }
-        println("After round $round, the monkeys are holding items with these worry levels:")
-        monkeys.forEach() { println(it) }
-    }
-    monkeys.forEach { monkey ->
-        println("Monkey ${monkey.number} inspected items ${monkey.numberOfInspections} times.")
+        if (whenToPrint(round)) {
+            println("After round $round, the monkeys are holding items with these worry levels:")
+            monkeys.forEach() { println(it) }
+            monkeys.forEach { monkey ->
+                println("Monkey ${monkey.number} inspected items ${monkey.numberOfInspections} times.")
+            }
+        }
     }
     val mostActiveMonkeys = monkeys.sortedByDescending { it.numberOfInspections }.subList(0, 2)
     println("mostActiveMonkeys: " + mostActiveMonkeys.map { "Monkey ${it.number}" })
@@ -87,15 +101,19 @@ fun part1(monkeys: List<Monkey>): Int {
 
 class Monkey(
     val number: Int,
-    val items: MutableList<Int>,
-    val operation: (Int) -> Int,
+    val items: MutableList<Long>,
+    val operation: (Long) -> Long,
     val operationDesc: String,
-    val divisibleBy: Int,
+    val divisibleBy: Long,
     val targetMonkeyIfTrue: Int,
     val targetMonkeyIfFalse: Int,
 ) {
 
-    var numberOfInspections = 0
+    var numberOfInspections = 0L
+
+    fun copy(): Monkey {
+        return Monkey(number, items.toMutableList(), operation, operationDesc, divisibleBy, targetMonkeyIfTrue, targetMonkeyIfFalse)
+    }
 
     override fun toString(): String {
         return "Monkey $number: ${items.joinToString()}"
